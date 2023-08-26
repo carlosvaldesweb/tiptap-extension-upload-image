353e13723ce1332962da4b75bdc36dc13509e234
Carlos Valdes <carlosvaldes096@gmail.com>
2023-08-25 20:58:22 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# TiptapExtensionUploadImage
+
+A fresh Tiptap extension.
+
+## Installation
+
+```bash
+npm install tiptap-extension-upload-image
+```
---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+// rollup.config.js
+
+const autoExternal = require("rollup-plugin-auto-external");
+const sourcemaps = require("rollup-plugin-sourcemaps");
+const commonjs = require("@rollup/plugin-commonjs");
+const babel = require("@rollup/plugin-babel");
+const scss = require("rollup-plugin-scss");
+
+const config = {
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+    },
+    {
+      file: "dist/index.js",
+      format: "esm",
+      exports: "named",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    autoExternal({ packagePath: "./package.json" }),
+    scss({
+        output: 'dist/upload-image.min.css',
+        outputStyle: "compressed"
+    }),
+    sourcemaps(),
+    babel(),
+    commonjs(),
+  ],
+};
+
+module.exports = config;
