a08df2ed773baa36f05d4306d4b2ec8b3166d1ab
Carlos Valdes <carlosvaldes096@gmail.com>
2024-05-22 14:41:59 -0600

    Added typescript support

--- rollup.config.js
+++ rollup.config.js
@@ -4,10 +4,11 @@ const autoExternal = require("rollup-plugin-auto-external");
 const sourcemaps = require("rollup-plugin-sourcemaps");
 const commonjs = require("@rollup/plugin-commonjs");
 const babel = require("@rollup/plugin-babel");
+const typescript = require("rollup-plugin-typescript2");
 const scss = require("rollup-plugin-scss");
 
 const config = {
-  input: "src/index.js",
+  input: "src/index.ts",
   output: [
     {
       file: "dist/index.cjs.js",
@@ -25,13 +26,52 @@ const config = {
   plugins: [
     autoExternal({ packagePath: "./package.json" }),
     scss({
-        output: 'dist/upload-image.min.css',
-        outputStyle: "compressed"
+      output: "dist/upload-image.min.css",
+      outputStyle: "compressed",
     }),
     sourcemaps(),
     babel(),
     commonjs(),
+    typescript(),
   ],
 };
 
 module.exports = config;
+
+// // rollup.config.js
+// import typescript from "@rollup/plugin-typescript";
+// import autoExternal from "rollup-plugin-auto-external";
+// import sourcemaps from "rollup-plugin-sourcemaps";
+// import commonjs from "@rollup/plugin-commonjs";
+// import babel from "@rollup/plugin-babel";
+// import scss from "rollup-plugin-scss";
+
+// const config = {
+//   input: "src/index.ts",
+//   output: [
+//     {
+//       file: "dist/index.cjs.js",
+//       format: "cjs",
+//       exports: "named",
+//       sourcemap: true,
+//     },
+//     {
+//       file: "dist/index.esm.js",
+//       format: "esm",
+//       sourcemap: true,
+//     },
+//   ],
+//   plugins: [
+//     autoExternal({ packagePath: "./package.json" }),
+//     scss({
+//       output: "dist/upload-image.min.css",
+//       outputStyle: "compressed",
+//     }),
+//     sourcemaps(),
+//     babel(),
+//     commonjs(),
+//     typescript(),
+//   ],
+// };
+
+// export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,111 +1,129 @@
 import { Plugin } from "@tiptap/pm/state";
 import { Decoration, DecorationSet } from "@tiptap/pm/view";
-import { mergeAttributes, Node, nodeInputRule } from "@tiptap/core";
+import Image from "@tiptap/extension-image";
+import { EditorView } from "@tiptap/pm/view";
+import { Schema } from "@tiptap/pm/model";
 import "./upload-image.css";
 
-export const inputRegex =
-  /(?:^|\s)(!\[(.+|:?)]\((\S+)(?:(?:\s+)["'](\S+)["'])?\))$/;
-let imagePreview = "";
-let uploadFn = async (file: File) => {};
+export interface UploadFn {
+  (file: File): Promise<string>;
+}
+export interface CustomImageOptions {
+  /**
+   * Controls if the image node should be inline or not.
+   * @default false
+   * @example true
+   */
+  inline: boolean;
 
-export const UploadImage = Node.create({
+  /**
+   * Controls if base64 images are allowed. Enable this if you want to allow
+   * base64 image urls in the `src` attribute.
+   * @default false
+   * @example true
+   */
+  allowBase64: boolean;
+
+  /**
+   * HTML attributes to add to the image element.
+   * @default {}
+   * @example { class: 'foo' }
+   */
+  HTMLAttributes: Record<string, any>;
+
+  /**
+   * Function to upload image
+   */
+  uploadFn: UploadFn;
+}
+
+declare module "@tiptap/core" {
+  interface Commands<ReturnType> {
+    customImage: {
+      /**
+       * Add an image
+       * @example
+       * editor
+       *   .commands
+       *   .addImage()
+       */
+      addImage: () => ReturnType;
+
+      /**
+       * Add an image
+       * @param options The image attributes
+       * @example
+       * editor
+       *   .commands
+       *   .setImage({ src: 'https://tiptap.dev/logo.png', alt: 'tiptap', title: 'tiptap logo' })
+       */
+      setImage: (options: {
+        src: string;
+        alt?: string;
+        title?: string;
+      }) => ReturnType;
+    };
+  }
+}
+
+let uploadFn: UploadFn;
+let imagePreview = "";
+
+const UploadImage = Image.extend<CustomImageOptions>({
   name: "uploadImage",
   onCreate() {
+    if (typeof this.options.uploadFn !== "function") {
+      console.warn("uploadFn should be a function");
+      return;
+    }
     uploadFn = this.options.uploadFn;
   },
   addOptions() {
     return {
-      inline: false,
-      HTMLAttributes: {},
-    };
-  },
-  inline() {
-    return this.options.inline;
-  },
-  group() {
-    return this.options.inline ? "inline" : "block";
-  },
-  draggable: true,
-  addAttributes() {
-    return {
-      src: {
-        default: null,
-      },
-      alt: {
-        default: null,
-      },
-      title: {
-        default: null,
+      ...this.parent?.(),
+      uploadFn: async () => {
+        return "";
       },
     };
   },
-  parseHTML() {
-    return [
-      {
-        tag: "img[src]",
-      },
-    ];
-  },
-  renderHTML({ HTMLAttributes }) {
-    return [
-      "img",
-      mergeAttributes(this.options.HTMLAttributes, HTMLAttributes),
-    ];
+  addProseMirrorPlugins() {
+    return [placeholderPlugin];
   },
   addCommands() {
     return {
-      addImage:
-        () =>
-        ({ commands }) => {
-          let fileHolder = document.createElement("input");
-          fileHolder.setAttribute("type", "file");
-          fileHolder.setAttribute("accept", "image/*");
-          fileHolder.setAttribute("style", "visibility:hidden");
-          document.body.appendChild(fileHolder);
+      ...this.parent?.(),
+      addImage: () => () => {
+        let fileHolder = document.createElement("input");
+        fileHolder.setAttribute("type", "file");
+        fileHolder.setAttribute("accept", "image/*");
+        fileHolder.setAttribute("style", "visibility:hidden");
+        document.body.appendChild(fileHolder);
 
-          let view = this.editor.view;
-          let schema = this.editor.schema;
+        const view = this.editor.view;
+        const schema = this.editor.schema;
 
-          fileHolder.addEventListener("change", (e: Event) => {
-            if (
-              view.state.selection.$from.parent.inlineContent &&
-              (<HTMLInputElement>e.target)?.files?.length
-            )
-              if (typeof uploadFn !== "function") {
-                console.warn("uploadFn should be a function");
-                return;
-              }
+        fileHolder.addEventListener("change", (e: Event) => {
+          if (
+            view.state.selection.$from.parent.inlineContent &&
+            (<HTMLInputElement>e.target)?.files?.length
+          ) {
             startImageUpload(
               view,
               (<HTMLInputElement>e.target)?.files![0],
               schema
             );
-            view.focus();
-          });
-          fileHolder.click();
-        },
+          }
+          view.focus();
+        });
+        fileHolder.click();
+        return true;
+      },
     };
-  },
-  addInputRules() {
-    return [
-      nodeInputRule({
-        find: inputRegex,
-        type: this.type,
-        getAttributes: (match) => {
-          const [, , alt, src, title] = match;
-
-          return { src, alt, title };
-        },
-      }),
-    ];
-  },
-  addProseMirrorPlugins() {
-    return [placeholderPlugin];
   },
 });
 
 //Plugin for placeholder
-let placeholderPlugin = new Plugin({
+const placeholderPlugin = new Plugin({
   state: {
     init() {
       return DecorationSet.empty;
@@ -148,7 +166,7 @@ function findPlaceholder(state, id) {
   return found?.length ? found[0].from : null;
 }
 
-function startImageUpload(view, file, schema) {
+function startImageUpload(view: EditorView, file: File, schema: Schema) {
   imagePreview = URL.createObjectURL(file);
   // A fresh object to act as the ID for this upload
   let id = {};
@@ -178,4 +196,240 @@ function startImageUpload(view, file, schema) {
     }
   );
 }
+export { UploadImage };
+
 export default UploadImage;
+
+// import { Plugin } from "@tiptap/pm/state";
+// import { Decoration, DecorationSet } from "@tiptap/pm/view";
+// import { mergeAttributes, Node, nodeInputRule } from "@tiptap/core";
+// import "./upload-image.css";
+
+// export interface UploadFn {
+//   (file: File): Promise<string>;
+// }
+// export interface CustomImageOptions {
+//   /**
+//    * Controls if the image node should be inline or not.
+//    * @default false
+//    * @example true
+//    */
+//   inline: boolean;
+
+//   /**
+//    * Controls if base64 images are allowed. Enable this if you want to allow
+//    * base64 image urls in the `src` attribute.
+//    * @default false
+//    * @example true
+//    */
+//   allowBase64: boolean;
+
+//   /**
+//    * HTML attributes to add to the image element.
+//    * @default {}
+//    * @example { class: 'foo' }
+//    */
+//   HTMLAttributes: Record<string, any>;
+
+//   /**
+//    * Function to upload image
+//    */
+//   uploadFn: UploadFn;
+// }
+
+// declare module "@tiptap/core" {
+//   interface Commands<ReturnType> {
+//     image: {
+//       /**
+//        * Add an image
+//        * @example
+//        * editor
+//        *   .commands
+//        *   .addImage()
+//        */
+//       addImage: () => ReturnType;
+//     };
+//   }
+// }
+
+// let uploadFn: UploadFn;
+// let imagePreview = "";
+// export const inputRegex =
+//   /(?:^|\s)(!\[(.+|:?)]\((\S+)(?:(?:\s+)["'](\S+)["'])?\))$/;
+
+// const UploadImage = Node.create<CustomImageOptions>({
+//   name: "uploadImage",
+//   onCreate() {
+//     if (typeof this.options.uploadFn !== "function") {
+//       console.warn("uploadFn should be a function");
+//       return;
+//     }
+//     uploadFn = this.options.uploadFn;
+//   },
+//   addOptions() {
+//     return {
+//       inline: false,
+//       HTMLAttributes: {},
+//       allowBase64: false,
+//       uploadFn: async () => {
+//         return "";
+//       },
+//     };
+//   },
+//   inline() {
+//     return this.options.inline;
+//   },
+//   group() {
+//     return this.options.inline ? "inline" : "block";
+//   },
+//   draggable: true,
+//   addAttributes() {
+//     return {
+//       src: {
+//         default: null,
+//       },
+//       alt: {
+//         default: null,
+//       },
+//       title: {
+//         default: null,
+//       },
+//     };
+//   },
+//   parseHTML() {
+//     return [
+//       {
+//         tag: "img[src]",
+//       },
+//     ];
+//   },
+//   renderHTML({ HTMLAttributes }) {
+//     return [
+//       "img",
+//       mergeAttributes(this.options.HTMLAttributes, HTMLAttributes),
+//     ];
+//   },
+//   addCommands() {
+//     return {
+//       addImage: () => () => {
+//         let fileHolder = document.createElement("input");
+//         fileHolder.setAttribute("type", "file");
+//         fileHolder.setAttribute("accept", "image/*");
+//         fileHolder.setAttribute("style", "visibility:hidden");
+//         document.body.appendChild(fileHolder);
+
+//         let view = this.editor.view;
+//         let schema = this.editor.schema;
+
+//         fileHolder.addEventListener("change", (e: Event) => {
+//           if (
+//             view.state.selection.$from.parent.inlineContent &&
+//             (<HTMLInputElement>e.target)?.files?.length
+//           )
+//             startImageUpload(
+//               view,
+//               (<HTMLInputElement>e.target)?.files![0],
+//               schema
+//             );
+//           view.focus();
+//         });
+//         fileHolder.click();
+//         return true;
+//       },
+//     };
+//   },
+//   addInputRules() {
+//     return [
+//       nodeInputRule({
+//         find: inputRegex,
+//         type: this.type,
+//         getAttributes: (match) => {
+//           const [, , alt, src, title] = match;
+
+//           return { src, alt, title };
+//         },
+//       }),
+//     ];
+//   },
+//   addProseMirrorPlugins() {
+//     return [placeholderPlugin];
+//   },
+// });
+
+// //Plugin for placeholder
+// let placeholderPlugin = new Plugin({
+//   state: {
+//     init() {
+//       return DecorationSet.empty;
+//     },
+//     apply(tr, set) {
+//       // Adjust decoration positions to changes made by the transaction
+//       set = set.map(tr.mapping, tr.doc);
+//       // See if the transaction adds or removes any placeholders
+//       let action = tr.getMeta(this);
+//       if (action && action.add) {
+//         let widget = document.createElement("div");
+//         let img = document.createElement("img");
+//         widget.classList.value = "image-uploading";
+//         img.src = imagePreview;
+//         widget.appendChild(img);
+//         let deco = Decoration.widget(action.add.pos, widget, {
+//           id: action.add.id,
+//         });
+//         set = set.add(tr.doc, [deco]);
+//       } else if (action && action.remove) {
+//         set = set.remove(
+//           set.find(undefined, undefined, (spec) => spec.id == action.remove.id)
+//         );
+//       }
+//       return set;
+//     },
+//   },
+//   props: {
+//     decorations(state) {
+//       return this.getState(state);
+//     },
+//   },
+// });
+
+// //Find the placeholder in editor
+// function findPlaceholder(state, id) {
+//   let decos = placeholderPlugin.getState(state);
+//   let found = decos?.find(undefined, undefined, (spec) => spec.id == id);
+
+//   return found?.length ? found[0].from : null;
+// }
+
+// function startImageUpload(view, file, schema) {
+//   imagePreview = URL.createObjectURL(file);
+//   // A fresh object to act as the ID for this upload
+//   let id = {};
+
+//   // Replace the selection with a placeholder
+//   let tr = view.state.tr;
+//   if (!tr.selection.empty) tr.deleteSelection();
+//   tr.setMeta(placeholderPlugin, { add: { id, pos: tr.selection.from } });
+//   view.dispatch(tr);
+//   uploadFn(file).then(
+//     (url) => {
+//       let pos = findPlaceholder(view.state, id);
+//       // If the content around the placeholder has been deleted, drop
+//       // the image
+//       if (pos == null) return;
+//       // Otherwise, insert it at the placeholder's position, and remove
+//       // the placeholder
+//       view.dispatch(
+//         view.state.tr
+//           .replaceWith(pos, pos, schema.nodes.uploadImage.create({ src: url }))
+//           .setMeta(placeholderPlugin, { remove: { id } })
+//       );
+//     },
+//     (e) => {
+//       // On failure, just clean up the placeholder
+//       view.dispatch(tr.setMeta(placeholderPlugin, { remove: { id } }));
+//     }
+//   );
+// }
+// export { UploadImage };
+
+// export default UploadImage;
